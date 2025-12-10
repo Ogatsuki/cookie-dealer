8645cd0ad02beee97c7feaa9075661eae8c8c6c2
ogatsuki <ogatsuki.masu.5fjo@gmail.com>
2025-12-10 17:51:41 +0900

    server.tsとactions.tsを作成中

---
+++ app/(auth)/actions.ts
@@ -0,0 +1,11 @@
+'user server';
+
+import createClient from 'utilities/supabase/server';
+
+const signin = async () => {
+  
+}
+
+const signout = async () => {
+
+}
--- utilities/supabase/server.ts
+++ utilities/supabase/server.ts
@@ -1,18 +1,25 @@
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
-const createClient = async () => {
+export const createClient = async () => {
   const cookieStore = await cookies();
 
   return createServerClient(
-    process.env.NEXT_SUPABASE_API_URL!,
-    process.env.NEXT_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_API_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll() {
+          try {
+            cookieToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
+          }
+          catch {
 
-      },
-      db: {
-        
+          }
+        }
       }
     }
   ) 
