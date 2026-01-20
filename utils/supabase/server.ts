4e015b1eb1eec3d44af593a0725058763c51d1c5
ogatsuki <ogatsuki.masu.5fjo@gmail.com>
2026-01-20 22:24:00 +0900

    1. request?: NextRequestを削除 2. 環境変数の名前があっていなかったので.env.localのほうを修正

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,9 +1,8 @@
 import { createServerClient } from '@supabase/ssr';
-import { type NextRequest } from 'next/server';
 import { cookies } from 'next/headers';
 
 
-export const createClient = async (request?: NextRequest) => {
+export const createClient = async () => {
   const cookieStore = await cookies();
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_API_URL!,
