0de10155fa969835c3a5d9725c7fb8ad82fe84c1
Ogatsuki <ogatsuki.masu.5fjo@gmail.com>
2026-01-27 13:30:36 +0900

    ログイン・登録フォーム英アウト調整中

--- app/(auth)/authForm.tsx
+++ app/(auth)/authForm.tsx
@@ -36,20 +36,20 @@ export default function AuthForm({ actionState, formData, response, meta }: t__A
             <p className="text-red-500/60 text-sm">{response.error}</p>
           )}
         </div>
-        <div className="flex flex-col h-100 mt-4 border border-gray-300 rounded-xl shadow-sm px-6 pt-4">
+        <div className="flex flex-col h-100 mt-8 border border-gray-300 rounded-xl shadow-sm px-5 pt-7">
           <form action={actionState.action} className="flex-1 flex flex-col pb-9 text-gray-900">
             <div className="flex flex-col space-y-5">
               <label className="flex flex-col space-y-2">
-                <h3 className=" text-gray-700 text-sm">メールアドレス</h3>
+                <h3 className=" text-gray-700 text-sm tracking-tight">メールアドレス</h3>
                 <input value={formData.email} onChange={e => formData.setEmail(e.target.value)} name='email' type="email" autoComplete="email" required className="border border-gray-300 rounded-md px-3 pt-0.5 pb-1 tracking-wide"/>
               </label>
               <label className="flex flex-col">
-                <h3 className="text-gray-700">パスワード</h3>
+                <h3 className="text-gray-700 text-sm tracking-tight">パスワード</h3>
                 <input value={formData.password} onChange={e => formData.setPassword(e.target.value)} name='password' type="text" autoComplete='current-password' required className="border border-gray-300 rounded-md px-3 mt-2 pt-0.5 pb-1 tracking-wide"/>
-                <span className="text-xs text-gray-400 mt-1.5">英数字6文字以上</span>
+                <span className="text-xs text-gray-400 mt-1.5 tracking-tight">英数字6文字以上</span>
               </label>
             </div>
-            <div className="mt-16 flex-1 flex items-end">
+            <div className="mt-14.5 flex-1 flex items-end">
               {!actionState.isPending ? (
                 <button className="duration-200 w-full py-4 bg-black/80 text-white rounded-lg">{meta.buttonText}</button>
               ) : (
